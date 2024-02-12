15e76848207c3ca76073d7808fda06f022736831
Karolis123212321 <159789459+Karolis123212321@users.noreply.github.com>
2024-02-12 16:38:05 +0200

    Update Projektas_KV.py

--- Projektas_KV.py
+++ Projektas_KV.py
@@ -3,17 +3,17 @@ def prideti (pirmas, antras):
     return pirmas + antras
 
 def atimti (pirmas, antras):
-    return pirmas + antras
+    return pirmas - antras
 
 def dauginti (pirmas, antras):
-    return pirmas + antras
+    return pirmas * antras
 
 def dalinti (pirmas, antras):
-    return pirmas + antras
+    return pirmas / antras
 
 pasirinkimas = input("Ka darysite? +|-|*|/")
-numeris_1 = int(input("Iveskite x"))
-numeris_2 = int(input("Iveskite y"))
+numeris_1 = int(input("Iveskite x: "))
+numeris_2 = int(input("Iveskite y: "))
 
 if pasirinkimas == '+':
     print("Atsakymas: ", prideti(numeris_1, numeris_2))
