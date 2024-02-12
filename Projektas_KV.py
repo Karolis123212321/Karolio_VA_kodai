4b0143bd47ab3c8e1098a8a1c2fb838a65b6e86f
Karolis123212321 <159789459+Karolis123212321@users.noreply.github.com>
2024-02-12 18:20:58 +0200

    Update Projektas_KV.py
    Naturalaus skaiciuotuvo 2 versija, naudojamos str.split komandos

--- Projektas_KV.py
+++ Projektas_KV.py
@@ -15,56 +15,57 @@ def saknis (pirmas):
     return math.sqrt(pirmas)
 
 print ("Pasirinkite skaiciuotuvo versija:")
-print ("N - naturalus")
-print ("P - Su paaiskinimais")
+print ("n - naturalus")
+print ("p - Su paaiskinimais")
 
 Versija = input("Pasirinkimas: ")
 
 
-if Versija == 'N':
-    pirmas_sk = int(input())
-    operatorius = input()
-    antras_sk = int(input())
-    if operatorius == '+':
-        print(" = ", pirmas_sk + antras_sk)
-    elif operatorius == '-':
-        print(" = ", pirmas_sk - antras_sk)
-    elif operatorius == '*':
-        print(" = ", pirmas_sk * antras_sk)
-    elif operatorius == '/':
-        print(" = ", pirmas_sk / antras_sk)
+if Versija == 'n':
+    tekstas = str(input())
+    a, b, c = tekstas.split(' ')
+    a = int(a)
+    c = int(c)
+    if b == '+':
+        print("Atsakymas: ",a + c)
+    elif b == '-':
+        print("Atsakymas: ",a - c)
+    elif b == '*':
+        print("Atsakymas: ",a * c)
+    elif b == '/':
+        print("Atsakymas: ",a / c)
 
+elif Versija == 'p':
+    while True:
+        print ("Pasirinkite kokia operacija norite daryti:")
+        print ("1 - sudetis")
+        print ("2 - atimtis:")
+        print ("3 - daugyba:")
+        print ("4 - dalyba:")
+        print ("5 - saknies istraukimas")
+        print ("6 - pabaigti darba")
 
-while True:
-    print ("Pasirinkite kokia operacija norite daryti:")
-    print ("1 - sudetis")
-    print ("2 - atimtis:")
-    print ("3 - daugyba:")
-    print ("4 - dalyba:")
-    print ("5 - saknies istraukimas")
-    print ("6 - pabaigti darba")
+        pasirinkimas = input("Pasirinkimas: ")
+        if pasirinkimas == '5':
+            skaicius = int(input("Iveskite skaiciu: "))
+            print ("Atsakymas: ", saknis(skaicius))
+            continue
+        if pasirinkimas == '6':
+            break
+        numeris_1 = int(input("Iveskite x: "))
+        numeris_2 = int(input("Iveskite y: "))
 
-    pasirinkimas = input("Pasirinkimas: ")
-    if pasirinkimas == '5':
-        skaicius = int(input("Iveskite skaiciu: "))
-        print ("Atsakymas: ", saknis(skaicius))
-        continue
-    if pasirinkimas == '6':
-        break
-    numeris_1 = int(input("Iveskite x: "))
-    numeris_2 = int(input("Iveskite y: "))
-
-    if pasirinkimas == '1':
-        print("Atsakymas: ", prideti(numeris_1, numeris_2))
-        
-    elif pasirinkimas == '2':
-        print("Atsakymas: ", atimti(numeris_1, numeris_2))
-        
-    elif pasirinkimas == '3':
-        print ("Atsakymas: ", dauginti(numeris_1, numeris_2))
-        
-    elif pasirinkimas == '4':
-        print("Atsakymas: ", dalinti(numeris_1, numeris_2))
+        if pasirinkimas == '1':
+            print("Atsakymas: ", prideti(numeris_1, numeris_2))
+            
+        elif pasirinkimas == '2':
+            print("Atsakymas: ", atimti(numeris_1, numeris_2))
+            
+        elif pasirinkimas == '3':
+            print ("Atsakymas: ", dauginti(numeris_1, numeris_2))
+            
+        elif pasirinkimas == '4':
+            print("Atsakymas: ", dalinti(numeris_1, numeris_2))
 
 
     
