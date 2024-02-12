a62d36f7b89b353e79277a0a72aa0eae9d748a5a
Karolis123212321 <159789459+Karolis123212321@users.noreply.github.com>
2024-02-12 17:34:48 +0200

    Update Projektas_KV.py
    Pridetas meniu

--- Projektas_KV.py
+++ Projektas_KV.py
@@ -11,20 +11,26 @@ def dauginti (pirmas, antras):
 def dalinti (pirmas, antras):
     return pirmas / antras
 
-pasirinkimas = input("Ka darysite? +|-|*|/")
+print ("Pasirinkite kokia operacija norite daryti:")
+print ("1 - sudetis")
+print ("2 - atimtis:")
+print ("3 - daugyba:")
+print ("4 - dalyba:")
+
+pasirinkimas = input("Pasirinkimas: ")
 numeris_1 = int(input("Iveskite x: "))
 numeris_2 = int(input("Iveskite y: "))
 
-if pasirinkimas == '+':
+if pasirinkimas == '1':
     print("Atsakymas: ", prideti(numeris_1, numeris_2))
     
-elif pasirinkimas == '-':
+elif pasirinkimas == '2':
     print("Atsakymas: ", atimti(numeris_1, numeris_2))
     
-elif pasirinkimas == '*':
+elif pasirinkimas == '3':
     print ("Atsakymas: ", dauginti(numeris_1, numeris_2))
     
-elif pasirinkimas == '/':
+elif pasirinkimas == '4':
     print("Atsakymas: ", dalinti(numeris_1, numeris_2))
     
 
