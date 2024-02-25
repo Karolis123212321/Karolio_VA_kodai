42cf92d78717ec1d9f1402e130f9751454534037
Karolis123212321 <159789459+Karolis123212321@users.noreply.github.com>
2024-02-25 22:53:14 +0200

    Update Projektas_KV.py
    Importuotas Flask

--- Projektas_KV.py
+++ Projektas_KV.py
@@ -1,4 +1,16 @@
 import math
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, world<p>"
+
+if __name__ == "__main__":
+    app.run()
+
+
 def prideti (pirmas, antras):
     return pirmas + antras
 
