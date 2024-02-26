b906726c11198432f4244ba10c17e008c43f28c0
Karolis123212321 <159789459+Karolis123212321@users.noreply.github.com>
2024-02-26 17:02:06 +0200

    Update Projektas_KV.py
    Pradetas kodas daryti is naujo, nuo pat pamatu

--- Projektas_KV.py
+++ Projektas_KV.py
@@ -1,64 +1,13 @@
 import math
-from flask import Flask, request, render_template_string
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def calculate():
-    if request.method == "POST":
-        operation = request.form.get("operation")
-        if operation == "saknis":
-            number = float(request.form.get("number"))
-            result = math.sqrt(number)
-            return render_template_string(HTML_TEMPLATE, result=int(result))
-        else:
-            number1 = float(request.form.get("number1"))
-            number2 = float(request.form.get("number2"))
-            if operation == "sudetis":
-                result = number1 + number2
-            elif operation == "atimtis":
-                result = number1 - number2
-            elif operation == "daugyba":
-                result = number1 * number2
-            elif operation == "dalyba":
-                result = number1 / number2
-            return render_template_string(HTML_TEMPLATE, result=result)
-    else:
-        return render_template_string(HTML_TEMPLATE)
 
-HTML_TEMPLATE = """
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Kalkuliatorius 1.0</title>
-</head>
-<body>
-  <h2>Kalkuliatorius 1.0</h2>
-  <form method="post">
-    <label for="operation">Pasirinkite ka darysite</label>
-    <select name="operation" id="operation">
-      <option value="sudetis">sudetis</option>
-      <option value="atimtis">Atimtis</option>
-      <option value="daugyba">Daugyba</option>
-      <option value="dalyba">Dalyba</option>
-      <option value="saknis">Saknis</option>
-    </select>
-    <br><br>
-    <input type="number" id="number1" name="number1" placeholder="1 numeris" required>
-    <input type="number" id="number2" name="number2" placeholder="2 numeris">
-    <input type="number" id="number" name="number" placeholder="Numeris sakniai">
-    <button type="submit">Skaiciuoti</button>
-  </form>
-  {% if result is defined %}
-  <h3>Atsakymas: {{ result }}</h3>
-  {% endif %}
-</body>
-</html>
-"""
 
-if __name__ == "__main__":
-    app.run(debug = True)
+
+
+
 
 
 def prideti (pirmas, antras):
@@ -75,6 +24,16 @@ def dalinti (pirmas, antras):
 
 def saknis (pirmas):
     return math.sqrt(pirmas)
+
+@app.route("/skaiciuokle")
+def skaiciuokle():
+    skaicius1 = request.args.get("sk1")
+    skaicius2 = request.args.get("sk2")
+    suma = prideti(int(skaicius1), int(skaicius2))
+    return f"{suma}"
+
+if __name__ == "__main__":
+    app.run()
 
 while True:
     print ("Pasirinkite skaiciuotuvo versija:")
